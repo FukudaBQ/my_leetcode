26abe63c54e7208aa9633d520aeb1f25661edbee
FukudaBQ <boweirocks@gmail.com>
2025-09-24 09:15:24 +0800

    leetcode32

---
+++ by_java/src/leetcode32.java
@@ -0,0 +1,31 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class leetcode32 {
+    public int longestValidParentheses(String s) {
+        int n = s.length();
+        if (n < 2) return 0;
+        int[] dp = new int[n];
+        int max = 0;
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + 2
+                            + (i - dp[i - 1] >= 2 ? dp[i - dp[i - 1] - 2] : 0);
+                }
+                max = Math.max(max, dp[i]);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        leetcode32 leetcode = new leetcode32();
+        int res = leetcode.longestValidParentheses("(()())(((()((())())");
+        System.out.println(res);
+    }
+}
