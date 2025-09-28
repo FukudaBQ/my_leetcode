a30d6227703f88e1c31886dd1f045132b254efed
FukudaBQ <boweirocks@gmail.com>
2025-09-28 15:42:38 +0800

    leetcode 1588

---
+++ by_golang/leetcode1588/leetcode.go
@@ -0,0 +1,24 @@
+package leetcode1588
+
+import "fmt"
+
+func sumOddLengthSubarrays(arr []int) int {
+	n := len(arr)
+	ans := 0
+	for i, x := range arr {
+		total := (i + 1) * (n - i)
+		odd := (total + 1) / 2
+		ans += x * odd
+	}
+	return ans
+}
+
+func Leetcode() {
+	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
+	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
+	s := "gopher"
+	fmt.Printf("Hello and welcome, %s!\n", s)
+
+	a := sumOddLengthSubarrays([]int{1, 4, 2, 5, 3})
+	fmt.Printf("is anagram: %v", a)
+}
