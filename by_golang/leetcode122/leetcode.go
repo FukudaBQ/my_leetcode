8be1b3402b81026b256a1dedccbb1acf5adb545f
FukudaBQ <boweirocks@gmail.com>
2025-10-15 09:40:25 +0800

    leetcode122

---
+++ by_golang/leetcode122/leetcode.go
@@ -0,0 +1,22 @@
+package leetcode122
+
+import "fmt"
+
+func maxProfit(prices []int) int {
+	res := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			res += prices[i] - prices[i-1]
+		}
+	}
+	return res
+}
+
+func Leetcode() {
+	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
+	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
+	s := "gopher"
+	fmt.Printf("Hello and welcome, %s!\n", s)
+	ans := maxProfit([]int{7, 1, 5, 3, 6, 4})
+	fmt.Printf("ans is: %v", ans)
+}
