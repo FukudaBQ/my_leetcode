8119e800fd84b16c20caabd69cc4bf18a11555a1
FukudaBQ <boweirocks@gmail.com>
2025-10-31 14:00:03 +0800

    leetcode189

---
+++ by_golang/leetcode189/leetcode.go
@@ -0,0 +1,28 @@
+package leetcode189
+
+import "fmt"
+
+func rotate(nums []int, k int) {
+	k = k % len(nums)
+	reverse(nums, 0, len(nums)-1)
+	reverse(nums, 0, k-1)
+	reverse(nums, k, len(nums)-1)
+}
+
+func reverse(nums []int, start, end int) {
+	for start < end {
+		nums[start], nums[end] = nums[end], nums[start]
+		start++
+		end--
+	}
+}
+
+func Leetcode() {
+	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
+	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
+	s := "gopher"
+	fmt.Printf("Hello and welcome, %s!\n", s)
+	a := []int{1, 2, 3, 4, 5, 6, 7}
+	rotate(a, 3)
+	fmt.Printf("is anagram: %v", a)
+}
