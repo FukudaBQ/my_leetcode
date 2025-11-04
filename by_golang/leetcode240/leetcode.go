3635e3eba741e18028d71cf702514e521817a651
FukudaBQ <boweirocks@gmail.com>
2025-11-04 15:20:06 +0800

    leetcode240

---
+++ by_golang/leetcode240/leetcode.go
@@ -0,0 +1,32 @@
+package leetcode240
+
+import "fmt"
+
+func searchMatrix(matrix [][]int, target int) bool {
+	i, j := 0, len(matrix[0])-1
+	for i < len(matrix) && j >= 0 {
+		//fmt.Printf("matrix[%d][%d]=%d\n", i, j, matrix[i][j])
+		if matrix[i][j] == target {
+			return true
+		}
+		if matrix[i][j] > target {
+			j--
+		} else {
+			i++
+		}
+	}
+	return false
+}
+
+func Leetcode() {
+	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
+	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
+	s := "gopher"
+	fmt.Printf("Hello and welcome, %s!\n", s)
+	x := [][]int{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}
+	for _, v := range x {
+		fmt.Println(v)
+	}
+	a := searchMatrix(x, 5)
+	fmt.Printf("is anagram: %v", a)
+}
