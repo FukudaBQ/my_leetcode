24a0ce6604fd898ad4ca96d198071190b1416dd2
FukudaBQ <boweirocks@gmail.com>
2025-11-20 16:14:43 +0800

    leetcode4

--- by_golang/leetcode4/leetcode.go
+++ by_golang/leetcode4/leetcode.go
@@ -5,12 +5,63 @@ import (
 )
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	if len(nums1) < len(nums2) {
+	if len(nums1) > len(nums2) {
 		nums1, nums2 = nums2, nums1
 	}
-	len1, len2 := len(nums1), len(nums2)
-	med := (len1 + len2) / 2
-	
+	m, n := len(nums1), len(nums2)
+
+	left, right := 0, m
+	halfLen := (m + n + 1) / 2
+
+	for left <= right {
+		i := (left + right) / 2
+		j := halfLen - i
+
+		if i < m && nums2[j-1] > nums1[i] {
+			left = i + 1
+		} else if i > 0 && nums1[i-1] > nums2[j] {
+			right = i - 1
+		} else {
+			var maxOfLeft int
+			if i == 0 {
+				maxOfLeft = nums2[j-1]
+			} else if j == 0 {
+				maxOfLeft = nums1[i-1]
+			} else {
+				maxOfLeft = max(nums1[i-1], nums2[j-1])
+			}
+
+			if (m+n)%2 == 1 {
+				return float64(maxOfLeft)
+			}
+
+			var minOfRight int
+			if i == m {
+				minOfRight = nums2[j]
+			} else if j == n {
+				minOfRight = nums1[i]
+			} else {
+				minOfRight = min(nums1[i], nums2[j])
+			}
+
+			return float64(maxOfLeft+minOfRight) / 2.0
+		}
+	}
+	return 0.0
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 func Leetcode() {
