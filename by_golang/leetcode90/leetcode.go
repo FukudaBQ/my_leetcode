9e0a6130964357a6bd369eb3b85690b2ed719e29
FukudaBQ <boweirocks@gmail.com>
2025-11-24 13:51:56 +0800

    leetcode90

---
+++ by_golang/leetcode90/leetcode.go
@@ -0,0 +1,41 @@
+package leetcode90
+
+import (
+	"fmt"
+	"sort"
+)
+
+func subsetsWithDup(nums []int) [][]int {
+	var res [][]int
+	var backtrack func(int, []int)
+
+	sort.Ints(nums)
+
+	backtrack = func(start int, path []int) {
+		temp := make([]int, len(path))
+		copy(temp, path)
+		res = append(res, temp)
+
+		for i := start; i < len(nums); i++ {
+			if i > start && nums[i] == nums[i-1] {
+				continue
+			}
+			path = append(path, nums[i])
+			backtrack(i+1, path)
+			path = path[:len(path)-1]
+		}
+	}
+
+	backtrack(0, []int{})
+	return res
+}
+
+func Leetcode() {
+	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
+	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
+	s := "gopher"
+	fmt.Printf("Hello and welcome, %s!\n", s)
+
+	a := subsetsWithDup([]int{3, 30, 34, 5, 9})
+	fmt.Println(a)
+}
